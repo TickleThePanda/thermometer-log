6d84b40f6331c0a05bbeebd2c455b1e0d69fd491
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2020-09-13 14:29:56 +0100

    Improve thermometer client

--- logger/black-pill+esp8266_ds18b20/src/ThermometerClient.cpp
+++ logger/black-pill+esp8266_ds18b20/src/ThermometerClient.cpp
@@ -6,6 +6,8 @@
 #define SERVER "thermometer.ticklethepanda.dev"
 #define PORT 443
 
+#define USER_AGENT "ticklethepanda-thermometer/0.0.1 test"
+
 #define AT_BAUD_RATE 115200
 
 #define DEBUG_LOG Serial
@@ -25,75 +27,105 @@ ThermometerClient::ThermometerClient(
 }
 
 void ThermometerClient::init() {
-  
+
   wifiSerial->begin(AT_BAUD_RATE);
 
   WiFi.init(wifiSerial, resetPin);
 
-  DEBUG_LOG.println("WiFi> Initialised");
+  DEBUG_LOG.println(F("WiFi> Initialised"));
 
   if (WiFi.status() == WL_NO_MODULE) {
 
     DEBUG_LOG.println();
-    DEBUG_LOG.println("Connection with ESP failed");
-    
+    DEBUG_LOG.println(F("Connection with ESP failed"));
+
     while (true);
   }
 
   WiFi.begin(ssid, pass);
-  
-  DEBUG_LOG.println("WiFi>Waiting for connection to WiFi");
+
+  DEBUG_LOG.println(F("WiFi> Waiting for connection to WiFi"));
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    DEBUG_LOG.print('.');
+    DEBUG_LOG.print(F("."));
     DEBUG_LOG.print(WiFi.status());
   }
 
-  DEBUG_LOG.println();
-  DEBUG_LOG.println("WiFi>Connected to WiFi network.");
+  DEBUG_LOG.println(F("WiFi> Connected to WiFi network."));
 }
 
 void ThermometerClient::send(const char *room, float temperature) {
 
-  DEBUG_LOG.println("WiFi> Starting connection to server...");
+  DEBUG_LOG.println(F("WiFi> Starting connection to server..."));
   if (client.connectSSL(SERVER, PORT)) {
-    DEBUG_LOG.println("WiFi> Connected to server");
+    DEBUG_LOG.println(F("WiFi> Connected to server"));
+    DEBUG_LOG.print(F("WiFi> "));
+    DEBUG_LOG.println(WiFi.status());
+    delay(300);
+    DEBUG_LOG.print(F("WiFi> "));
+    DEBUG_LOG.println(WiFi.status());
 
-    delay(100);
+    char tempAsText[9];
+    dtostrf(temperature, 8, 3, tempAsText);
 
-    client.print("POST /rooms/");
+    client.print(F("POST /rooms/"));
     client.print(room);
-    client.println("/log HTTP/1.1");
-    client.print("Host: ");
+    client.println(F("/log HTTP/1.1"));
+
+    client.print(F("Host: "));
     client.println(SERVER);
-    client.println("User-Agent: ticklethepanda-thermometer/0.0.1 test");
-    client.print("Authorization: Bearer ");
+
+    client.print(F("User-Agent: "));
+    client.println(USER_AGENT);
+
+    client.print(F("Authorization: Bearer "));
     client.println(thermometerApiKey);
-    client.println("Content-Type: application/json");
-    client.println("Connection: close");
+
+    client.println(F("Connection: close"));
+
+    client.println(F("Content-Type: application/json"));
+
+    client.print(F("Content-Length: "));
+    client.println(15 + 8 + 1);
+
     client.println();
-    client.print("{ \"temperature\":");
-    client.print(temperature);
-    client.println("}");
-    client.println();
+
+    client.print(F("{\"temperature\":"));
+    client.print(tempAsText);
+    client.println(F("}"));
+
   } else {
-    
-    DEBUG_LOG.println("WiFi> Conneciton failed");
+
+    DEBUG_LOG.println(F("WiFi> Conneciton failed"));
+
+    return;
   }
+
+  uint8_t firstLineData[12];
 
   // if there are incoming bytes available
   // from the server, read them and print them
   while (client.connected()) {
-    while (client.available()) {
-      char c = client.read();
-
-      DEBUG_LOG.write(c);
+    if (client.available() >= 12) {
+      client.read(firstLineData, 12);
+      break;
     }
   }
 
-  DEBUG_LOG.println("WiFi>Closing connection");
+  char *firstLine = (char*) firstLineData;
 
+  strtok(firstLine, " ");
+  char *statusCode = strtok(NULL, " ");
+
+  if (strcmp(statusCode, "204") == 0) {
+    DEBUG_LOG.println(F("WiFi> Successfully updated temperature"));
+  } else {
+    DEBUG_LOG.println(F("WiFi> Failed to update temperature"));
+  }
+
+  DEBUG_LOG.println(F("WiFi> Closing connection"));
+
+  client.flush();
   client.stop();
 
 }
-
