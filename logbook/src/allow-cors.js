a1a13a9bd2eadca9f09a66c2d8881e4df8fac3be
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2020-10-08 22:03:33 +0100

    Add max-age to CORS

--- logbook/src/allow-cors.js
+++ logbook/src/allow-cors.js
@@ -13,6 +13,7 @@ module.exports = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
     res.setHeader('Timing-Allow-Origin', origin);
+    res.setHeader('Access-Control-Max-Age', '86400');
     res.setHeader(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
